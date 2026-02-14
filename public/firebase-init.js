939a5917387c7ad383741c824eff4032ae2017e7
Rick from88 <rick@example.com>
2026-02-14 20:02:50 +0100

    chore: set firebase project id and add public/firebase-init.js (user config)

---
+++ public/firebase-init.js
@@ -0,0 +1,21 @@
+// Firebase init (config provided by user)
+window.firebaseConfig = {
+  apiKey: "AIzaSyD14Iwp8m6ivTTaA3Bft7I1-xL8tc7NUzk",
+  authDomain: "akin-bestellwesen.firebaseapp.com",
+  projectId: "akin-bestellwesen",
+  storageBucket: "akin-bestellwesen.firebasestorage.app",
+  messagingSenderId: "461919576268",
+  appId: "1:461919576268:web:e495915c145681cb81a269",
+  measurementId: "G-0GNGTXZSG9"
+};
+
+// Usage:
+// 1) Include Firebase SDKs in your HTML (CDN) before this script, e.g.:
+// <script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics-compat.js"></script>
+// 2) Then include this file and initialize:
+// <script src="/firebase-init.js"></script>
+// <script>
+//   const app = firebase.initializeApp(window.firebaseConfig);
+//   try { firebase.getAnalytics(app); } catch(e) { /* analytics optional */ }
+// </script>
